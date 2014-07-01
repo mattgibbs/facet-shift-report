7053ee6a35dbe587c8ab7bedca18e884011af84e
David Wright <wrightdj@slac.stanford.edu>
2014-07-01 03:47:22 -0700

    Finished database migration scripts

--- db_migrate.py
+++ db_migrate.py
@@ -4,8 +4,7 @@ from app import db
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 
-migration = SQLALCHEMY_MIGRATE_REPO + '/versions/%03d_migration.py' % 
-	(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)+1)
+migration = SQLALCHEMY_MIGRATE_REPO + '/versions/%03d_migration.py' % (api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)+1)
 
 
 tmp_module = imp.new_module('old_model')
