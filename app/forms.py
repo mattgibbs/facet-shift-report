a8e7390c74dc86112704fb1f1964598f209be4aa
David Wright <wrightdj@slac.stanford.edu>
2014-07-11 11:48:52 -0700

    Added in user group selection field. Communicates with db to get users

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,15 @@
 from flask.ext.wtf import Form
-from wtforms import SelectField, TextField, BooleanField, TextAreaField
+from wtforms import fields
 from wtforms.validators import Required
 
+class Body(Form):
+	goals		= fields.TextAreaField('Goals', validators = [Required()])
+	progress	= fields.TextAreaField('Progress', validators = [Required()])
+
 class LoginForm(Form):
-	user 		= SelectField('User Group')
-	openid 		= TextField('openid', validators = [Required()])
-	goals 		= TextAreaField('Goals', validators = [Required()])
+	user 		= fields.SelectField('User Group', choices=[('error', 'FAILED TO LOAD USER GROUPS')])
+	textboxes 	= fields.FieldList(fields.TextAreaField('Name', validators = [Required()]), min_entries=1)
+	body		= fields.FormField(Body)
+	
+	def setChoices(self, inputChoices):
+		self.user.choices = inputChoices
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect
-from app import app
+from app import app, db, models
 from forms import LoginForm
 
 @app.route('/')
@@ -19,7 +19,12 @@ def index():
 
 @app.route('/shift_summary_form/', methods=['GET', 'POST'])
 def shift_summary_form():
+	choices = []
+	users = models.User.query.all()
+	for u in users:
+		choices.append((str(u.id), u.name))
 	form = LoginForm()
+	form.setChoices(choices)
 	if form.validate_on_submit():
 		flash("WHATUP")
 		return redirect('/')
