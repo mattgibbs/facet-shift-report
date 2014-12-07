eda1d2b3e6652113dc361fe143d5a12f738ac593
Matt Gibbs <mgibbs@gatech.edu>
2014-12-07 05:47:39 -0800

    Check if the production environment variable is set, don't load production config if it isn't.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.config.from_envvar('FACETSHIFTREPORT_SETTINGS')
+try:
+	os.environ["FACETSHIFTREPORT_SETTINGS"]
+	app.config.from_envvar('FACETSHIFTREPORT_SETTINGS')
+	print "Running in production mode."
+except KeyError:
+	print "Running in development mode."
+	
+
 db = SQLAlchemy(app)
 
 from app import views, models
