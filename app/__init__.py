975bcad5187a460606d2fdc06c398847a67bdbd7
Matt Gibbs <mgibbs@gatech.edu>
2014-12-07 05:35:13 -0800

    Add a production config file that gets read if an environment variable points to it.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config.from_envvar('FACETSHIFTREPORT_SETTINGS')
 db = SQLAlchemy(app)
 
 from app import views, models
---
+++ config_prod.py
@@ -0,0 +1,3 @@
+#These will get loaded if the 'FACETSHIFTREPORT_SETTINGS' environment variable points to this file.
+SERVER_NAME = "ad-ops.slac.stanford.edu"
+APPLICATION_ROOT = "/facet-shift-report"
