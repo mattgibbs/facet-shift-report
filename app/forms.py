a5a5134512786b55ace7943541b5bb300b3f0e42
Matt Gibbs <mgibbs@gatech.edu>
2015-02-26 15:07:24 -0800

    Remove the default timestamps for the shift start and end times, so that users are forced to enter them, rather than just leaving the defaults.

--- app/forms.py
+++ app/forms.py
@@ -17,8 +17,8 @@ class ShiftForm(Form):
 
 	# USE DATETIME FORMAT OF %Y-%m-%dT%H:%M:%S FOR FILE NAME
 	postTime		= fields.DateTimeField('Time to Post to E-Log', default=datetime.now, format="%Y-%m-%d %H:%M:%S", validators=[Required()])
-	shiftStart		= fields.DateTimeField('Shift Start Time', default=datetime.now, format="%Y-%m-%d %H:%M:%S", validators=[Required()])
-	shiftEnd		= fields.DateTimeField('Shift End Time', default=datetime.now, format="%Y-%m-%d %H:%M:%S", validators=[Required()])
+	shiftStart		= fields.DateTimeField('Shift Start Time', format="%Y-%m-%d %H:%M:%S", validators=[Required()])
+	shiftEnd		= fields.DateTimeField('Shift End Time', format="%Y-%m-%d %H:%M:%S", validators=[Required()])
 
 	goals			= fields.TextAreaField('Goals')
 	progress		= fields.TextAreaField('Progress')
